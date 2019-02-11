6158dad80e12419504b3fa374bb9a38043891361
Wendy <wentalau@gmail.com>
2019-02-11 15:12:43 +0700

    implementation service.

--- src/main/java/com/fivaa/service/impl/FivaServiceImpl.java
+++ src/main/java/com/fivaa/service/impl/FivaServiceImpl.java
@@ -17,7 +17,23 @@ public class FivaServiceImpl implements FivaService{
 
     @Override
     public List<String> process(int n) {
-        
+        List<String> result = new ArrayList<>();
+        if(n > 1){
+            for(int i = 1; i<=n ; i++){
+               String output = "";
+                for(int j=1; j<=2 ;j++){
+                    output+= String.valueOf(n-i);
+                }
+                for(int k=0; k<=n-i; k++){
+                    output+= String.valueOf(n+2-i);
+                }
+                result.add(output);
+            }
+        }
+        else{
+            result.add("Minimal input adalah 1");
+        }
+        return result;
     }
     
 }
